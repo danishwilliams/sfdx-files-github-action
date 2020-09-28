0d497e14111a142185597c69bf4b5324a63bdd68
Dane Williams <dane.williams@ibm.com>
2020-09-28 15:36:10 +0200

    Support other git change types and return test files

--- index.js
+++ index.js
@@ -2,29 +2,37 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
     const changedFiles = JSON.parse(core.getInput('changed-files'));
-    console.log(`changedFiles json ${JSON.stringify(changedFiles)}`);
-    console.log('changedFiles raw', changedFiles);
+    const addedFiles = JSON.parse(core.getInput('added-files'));
+    const renamedFiles = JSON.parse(core.getInput('renamed-files'));
+
+    const allFiles = [...changedFiles, ...addedFiles, ...renamedFiles];
+
+    console.log('allFiles raw', allFiles);
 
     let sfFilePaths = [];
-    for (let index = 0; index < changedFiles.length; index++) {
-        const filePath = changedFiles[index];
-
+    let sfTestFilePaths = [];
+    allFiles.forEach(filePath => {
         if (filePath.indexOf('force-app/main/default') === 0)
             sfFilePaths.push(filePath);
-    }
-    /* changedFiles.forEach(filePath => {
-        if (filePath.indexOf('force-app/main/default') === 0)
-            sfFilePaths.push(filePath);
-    }); */
 
-    console.log(sfFilePaths.join(','));
-    core.setOutput("files-to-deploy", sfFilePaths.join(','));
+        var filename = filePath.replace(/^.*[\\\/]/, '');
+        if (filename.toLower().includes('test'))
+            sfTestFilePaths.push(filePath);
+    });
+
+    var sfFilePathsComma = sfFilePaths.join(',')
+    var sfTestFilePathsComma = sfTestFilePaths.join(',')
+
+    console.log('sfFilePathsComma', sfFilePathsComma);
+    console.log('sfTestFilePathsComma', sfTestFilePathsComma);
+    
+    core.setOutput("files-to-deploy", sfFilePathsComma);
+    core.setOutput("test-files", sfTestFilePathsComma);
 
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
