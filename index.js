b4dcecd0a741a476c8445896ac9c490c2ffa9287
Dane Williams <dane.williams@ibm.com>
2020-09-28 15:03:56 +0200

    Add logging

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const github = require('@actions/github');
 try {
     // `who-to-greet` input defined in action metadata file
     const changedFiled = core.getInput('changed-files');
-    console.log(`changedFiled ${JSON.stringify(changedFiled)}!`);
+    console.log(`changedFiled ${JSON.stringify(changedFiled)}`);
+
+    let sfFilePaths = [];
+    changedFiles.forEach(filePath => {
+        if (filePath.indexOf('force-app/main/default') === 0)
+            sfFilePaths.push(filePath);
+    });
+
+    console.log(sfFilePaths.join(','));
     
-    //core.setOutput("time", time);
+    core.setOutput("files-to-deploy", sfFilePaths.join(','));
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
