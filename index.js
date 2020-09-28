9fcf25d6aa30e4c402f576033c2069fb9e37c6c1
Dane Williams <dane.williams@ibm.com>
2020-09-28 15:19:41 +0200

    Debug fixes

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const changedFiles = core.getInput('changed-files');
+    const changedFiles = JSON.parse(core.getInput('changed-files'));
     console.log(`changedFiles json ${JSON.stringify(changedFiles)}`);
     console.log('changedFiles raw', changedFiles);
 
     let sfFilePaths = [];
-    changedFiles.forEach(filePath => {
+    for (let index = 0; index < changedFiles.length; index++) {
+        const filePath = changedFiles[index];
+
         if (filePath.indexOf('force-app/main/default') === 0)
             sfFilePaths.push(filePath);
-    });
+    }
+    /* changedFiles.forEach(filePath => {
+        if (filePath.indexOf('force-app/main/default') === 0)
+            sfFilePaths.push(filePath);
+    }); */
 
     console.log(sfFilePaths.join(','));
     core.setOutput("files-to-deploy", sfFilePaths.join(','));
